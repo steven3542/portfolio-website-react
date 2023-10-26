1ef1475cd4b063909e317f1b9ba3983cd4e94cd1
Steven Zavala <55272248+steven3542@users.noreply.github.com>
2023-10-26 17:05:55 -0400

    added url to social icons deleted facebook icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -73,13 +73,11 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#">
+                <a href="https://www.linkedin.com/in/steven-zavala-b7b339265/">
                   <img src={navIcon1} alt="" />
                 </a>
-                <a href="#">
-                  <img src={navIcon2} alt="" />
-                </a>
-                <a href="#">
+
+                <a href="https://www.instagram.com/steve_pk672/">
                   <img src={navIcon3} alt="" />
                 </a>
               </div>
